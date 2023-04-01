7e6b865c6c9fae4c6a9f60db4c9fb398f9bbbe6b
Alexey Bolshakov <bad2600@outlook.com>
2023-04-01 19:21:26 +0300

    UPD

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -1,14 +1,6 @@
 ﻿#include "stdafx.h"
 #include "Print.h"
 #include "PushElemToArray.h"
-template <typename T1>void erase(T1*& arr, int& N, int pos)
-{
-	T1* arr2 = new T1[N-1];
-	for (int i = 0; i < N; i++) i < pos ? arr2[i] = arr[i] : arr2[i] = arr[i+1];
-	N--;
-	delete[] arr;
-	arr = arr2;
-}
 
 void main()
 {
@@ -33,6 +25,7 @@ void main()
 	insert(arr, n, n/2, 26);
 	Print(arr, n);
 	erase(arr, n, 0);
+	insert(arr, n, 10, 26);
 	Print(arr, n);
 	cout << endl;
 	delete[] arr;
